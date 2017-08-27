8ca71d3b32d2a11e9dcbdf16df0ef1b7b5ffbb9a
chunsiyang <chunsi.yang@hotmail.com>
2017-08-27 20:29:51 +0800

    增加将查询结果转发到seultSet.jsp进行显示

--- src/com/RacingDroneWILI/servlet/SearchAllReq.java
+++ src/com/RacingDroneWILI/servlet/SearchAllReq.java
@@ -12,42 +12,57 @@ import java.util.List;
  */
 public class SearchAllReq extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        response.setContentType("text/html");
         String url=request.getRequestURL().toString();
         ServletContext context= getServletContext();
+        request.setAttribute("motoList",null);
+        request.setAttribute("electroSpeedRegulatorList",null);
+        request.setAttribute("frameList",null);
+        request.setAttribute("imageTransmissionList",null);
+        request.setAttribute("camList",null);
+        request.setAttribute("flightControlList",null);
+        request.setAttribute("antennaList",null);
+        request.setAttribute("propList",null);
         if(url==context.getInitParameter("moto"))
         {
             List<Moto> list=new MotoDAOProxy().findAll();
+            request.setAttribute("motoList",list);
         }
         if(url==context.getInitParameter("esc"))
         {
             List<ElectroSpeedRegulator> list=new ElectroSpeedRegulatorDaoProxy().findAll();
+            request.setAttribute("motoList",list);
         }
         if(url==context.getInitParameter("fra"))
         {
             List<Frame> list=new FrameDaoProxy().findAll();
+            request.setAttribute("motoList",list);
         }
         if(url==context.getInitParameter("it"))
         {
             List<ImageTransmission> list=new ImageTransmissionDaoProxy().findAll();
+            request.setAttribute("motoList",list);
         }
         if(url==context.getInitParameter("cam"))
         {
             List<Cam> list=new CamDAOProxy().findAll();
+            request.setAttribute("motoList",list);
         }
         if(url==context.getInitParameter("fc"))
         {
             List<FlightControl> list=new FlightControlDaoProxy().findAll();
+            request.setAttribute("motoList",list);
         }
         if(url==context.getInitParameter("ant"))
         {
             List<Antenna> list= new AntennaDaoProxy().findAll();
+            request.setAttribute("motoList",list);
         }
         if(url==context.getInitParameter("prop"))
         {
             List<Prop> list=new PropDaoProxy().findAll();
+            request.setAttribute("motoList",list);
         }
-
+        request.getRequestDispatcher("/resultSet.jsp").forward(request,response);
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
