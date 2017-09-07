7a91ce6ddf5216959f262003b7cf8a34c84263de
chunsiyang <chunsi.yang@hotmail.com>
2017-09-07 21:08:00 +0800

    创建taglib将javabean中的bool类型属性转换为支持和不支持文字

---
+++ src/main/java/com/RacingDroneWIKI/taglibs/BoolToString.java
@@ -0,0 +1,38 @@
+package com.RacingDroneWIKI.taglibs;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import javax.servlet.jsp.JspContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+/**
+ * Created by asus-pc on 2017/9/7.
+ */
+public class BoolToString extends SimpleTagSupport {
+    private String bool;
+
+    public String getBoolBool() {
+        return bool;
+    }
+
+    public void setBool(String bool) {
+        this.bool = bool;
+    }
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        super.doTag();
+        JspContext context=getJspContext();
+        JspWriter out=context.getOut();
+        if(bool.equals("true"))
+        {
+            out.print("支持");
+        }
+        else {
+            out.print("不支持");
+        }
+    }
+}
