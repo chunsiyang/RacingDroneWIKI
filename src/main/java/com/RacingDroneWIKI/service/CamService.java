5afb3b0537c0e797779f20d525b4e8536e7da0cc
chunsiyang <chunsi.yang@hotmail.com>
2017-11-13 09:00:04 +0800

    完成对分类查找的ssm重构

---
+++ src/main/java/com/RacingDroneWIKI/service/CamService.java
@@ -0,0 +1,56 @@
+package com.RacingDroneWIKI.service;
+
+import java.util.*;
+
+import com.RacingDroneWIKI.dao.CamDao;
+import com.RacingDroneWIKI.pojo.Cam;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CamService implements CamDao {
+	@Autowired
+	CamDao camDao;
+	public int addCam(Cam cam) {
+		int res=0;
+		res=camDao.addCam(cam);
+		return res;
+	}
+
+	public int deleteCam(Cam cam) {
+		int res=0;
+		res=camDao.deleteCam(cam);
+		return res;
+	}
+
+	public int updataCam(Cam cam) {
+		int res=0;
+		res=camDao.updataCam(cam);
+		return res;
+	}
+
+	public List<Cam> findAll() {
+		List<Cam> res;
+		res=camDao.findAll();
+		if(res.isEmpty())
+			return null;
+		return res;
+	}
+
+	public List<Cam> findByModel(String model) {
+		List<Cam> res;
+		res=camDao.findByModel(model);
+		if (res.isEmpty())
+			return null;
+		return res;
+	}
+
+	@Override
+	public Cam findDetail(String model) {
+		Cam res;
+		res=camDao.findDetail(model);
+		return res;
+	}
+
+}
