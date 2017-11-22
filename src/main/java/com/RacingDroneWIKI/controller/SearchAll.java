2efd536aa4ec505e7e63121bc1f75ee576fff592
chunsiyang <chunsi.yang@hotmail.com>
2017-11-22 22:48:59 +0800

    完成搜索查询和列表查询的分页显示 考虑到兼容老旧代码采用在服务器端进行分页并未从数据库端进行分页

--- src/main/java/com/RacingDroneWIKI/controller/SearchAll.java
+++ src/main/java/com/RacingDroneWIKI/controller/SearchAll.java
@@ -5,7 +5,9 @@ import com.RacingDroneWIKI.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,86 +15,169 @@ import java.util.List;
  * Created by user on 2017/11/7.
  */
 @Controller
-public class SearchAll  {
+public class SearchAll {
+    final int PAGENUMBER = 10;
+    int pages;
+    int first;
+    int last;
     @Autowired
     MotoService motoService;
-    @RequestMapping(value = "/moto")
-    public String motoList(Model model)
-    {
-        List<Moto> list=motoService.findAll();
-        model.addAttribute("motoList",list);
-        return "resultSet";
-    }
     @Autowired
     ElectroSpeedRegulatorService electroSpeedRegulatorServiicer;
-    @RequestMapping(value = "/electroSpeedRegulator")
-    public String electroSpeedRegulatorList(Model model){
-        List<ElectroSpeedRegulator> list =electroSpeedRegulatorServiicer.findAll();
-        model.addAttribute("electroSpeedRegulatorList",list);
-        return "resultSet";
-    }
     @Autowired
     FrameService frameService;
-    @RequestMapping(value = "/frame")
-    public String frameList(Model model){
-        List<Frame> list =frameService.findAll();
-        model.addAttribute("frameList",list);
-        return "resultSet";
-    }
     @Autowired
     ImageTransmissionService imageTransmissionService;
-    @RequestMapping(value = "/imageTransmission")
-    public String imageTransmissionList(Model model){
-        List<ImageTransmission> list =imageTransmissionService.findAll();
-        model.addAttribute("imageTransmissionList",list);
-        return "resultSet";
-    }
     @Autowired
     CamService camService;
-    @RequestMapping(value = "/cam")
-    public String camList(Model model){
-        List<Cam> list =camService.findAll();
-        model.addAttribute("camList",list);
-        return "resultSet";
-    }
     @Autowired
     FlightControlService flightControlService;
-    @RequestMapping(value = "/flightControl")
-    public String flightControlList(Model model){
-        List<FlightControl> list =flightControlService.findAll();
-        model.addAttribute("flightControlList",list);
-        return "resultSet";
-    }
     @Autowired
     AntennaService antennaService;
-    @RequestMapping(value = "/antenna")
-    public String antennaList(Model model){
-        List<Antenna> list =antennaService.findAll();
-        model.addAttribute("antennaList",list);
-        return "resultSet";
-    }
     @Autowired
     PowerHubService powerHubService;
-    @RequestMapping(value = "/powerHub")
-    public String powerHubList(Model model){
-        List<PowerHub> list =powerHubService.findAll();
-        model.addAttribute("powerHubList",list);
-        return "resultSet";
-    }
     @Autowired
     PropService propService;
-    @RequestMapping(value = "/prop")
-    public String propList(Model model){
-        List<Prop> list =propService.findAll();
-        model.addAttribute("propList",list);
-        return "resultSet";
-    }
     @Autowired
     BatteryService batteryService;
-    @RequestMapping(value = "/battery")
-    public String batteryList(Model model){
-        List<Battery> list =batteryService.findAll();
-        model.addAttribute("batteryList",list);
+
+    @RequestMapping(value = "/moto_{page}")
+    public String motoList(Model model, @PathVariable(value = "page") int page) {
+        List<Moto> list = motoService.findAll();
+        pages = list.size() % PAGENUMBER != 0 ? list.size() / PAGENUMBER + 1 : list.size() / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = list.size() > (first + PAGENUMBER) ? first + PAGENUMBER : list.size();
+        list = list.subList(first, last);
+        model.addAttribute("uri", "/moto_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("motoList", list);
+        return "resultSet";
+    }
+
+    @RequestMapping(value = "/electroSpeedRegulator_{page}")
+    public String electroSpeedRegulatorList(Model model, @PathVariable(value = "page") int page) {
+        List<ElectroSpeedRegulator> list = electroSpeedRegulatorServiicer.findAll();
+        pages = list.size() % PAGENUMBER != 0 ? list.size() / PAGENUMBER + 1 : list.size() / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = list.size() > (first + PAGENUMBER) ? first + PAGENUMBER : list.size();
+        list = list.subList(first, last);
+        model.addAttribute("uri", "/electroSpeedRegulator_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("electroSpeedRegulatorList", list);
+        return "resultSet";
+    }
+
+    @RequestMapping(value = "/frame_{page}")
+    public String frameList(Model model, @PathVariable(value = "page") int page) {
+        List<Frame> list = frameService.findAll();
+        pages = list.size() % PAGENUMBER != 0 ? list.size() / PAGENUMBER + 1 : list.size() / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = list.size() > (first + PAGENUMBER) ? first + PAGENUMBER : list.size();
+        list = list.subList(first, last);
+        model.addAttribute("uri", "/frame_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("frameList", list);
+        return "resultSet";
+    }
+
+    @RequestMapping(value = "/imageTransmission_{page}")
+    public String imageTransmissionList(Model model, @PathVariable(value = "page") int page) {
+        List<ImageTransmission> list = imageTransmissionService.findAll();
+        pages = list.size() % PAGENUMBER != 0 ? list.size() / PAGENUMBER + 1 : list.size() / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = list.size() > (first + PAGENUMBER) ? first + PAGENUMBER : list.size();
+        list = list.subList(first, last);
+        model.addAttribute("uri", "/imageTransmission_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("imageTransmissionList", list);
+        return "resultSet";
+    }
+
+    @RequestMapping(value = "/cam_{page}")
+    public String camList(Model model, @PathVariable(value = "page") int page) {
+        List<Cam> list = camService.findAll();
+        pages = list.size() % PAGENUMBER != 0 ? list.size() / PAGENUMBER + 1 : list.size() / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = list.size() > (first + PAGENUMBER) ? first + PAGENUMBER : list.size();
+        list = list.subList(first, last);
+        model.addAttribute("uri", "/cam_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("camList", list);
+        return "resultSet";
+    }
+
+    @RequestMapping(value = "/flightControl_{page}")
+    public String flightControlList(Model model, @PathVariable(value = "page") int page) {
+        List<FlightControl> list = flightControlService.findAll();
+        pages = list.size() % PAGENUMBER != 0 ? list.size() / PAGENUMBER + 1 : list.size() / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = list.size() > (first + PAGENUMBER) ? first + PAGENUMBER : list.size();
+        list = list.subList(first, last);
+        model.addAttribute("uri", "/flightControl_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("flightControlList", list);
+        return "resultSet";
+    }
+
+    @RequestMapping(value = "/antenna_{page}")
+    public String antennaList(Model model, @PathVariable(value = "page") int page) {
+        List<Antenna> list = antennaService.findAll();
+        pages = list.size() % PAGENUMBER != 0 ? list.size() / PAGENUMBER + 1 : list.size() / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = list.size() > (first + PAGENUMBER) ? first + PAGENUMBER : list.size();
+        list = list.subList(first, last);
+        model.addAttribute("uri", "/antenna_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("antennaList", list);
+        return "resultSet";
+    }
+
+    @RequestMapping(value = "/powerHub_{page}")
+    public String powerHubList(Model model, @PathVariable(value = "page") int page) {
+        List<PowerHub> list = powerHubService.findAll();
+        pages = list.size() % PAGENUMBER != 0 ? list.size() / PAGENUMBER + 1 : list.size() / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = list.size() > (first + PAGENUMBER) ? first + PAGENUMBER : list.size();
+        list = list.subList(first, last);
+        model.addAttribute("uri", "/powerHub_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("powerHubList", list);
+        return "resultSet";
+    }
+
+    @RequestMapping(value = "/prop_{page}")
+    public String propList(Model model, @PathVariable(value = "page") int page) {
+        List<Prop> list = propService.findAll();
+        pages = list.size() % PAGENUMBER != 0 ? list.size() / PAGENUMBER + 1 : list.size() / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = list.size() > (first + PAGENUMBER) ? first + PAGENUMBER : list.size();
+        list = list.subList(first, last);
+        model.addAttribute("uri", "/prop_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("propList", list);
+        return "resultSet";
+    }
+
+    @RequestMapping(value = "/battery_{page}")
+    public String batteryList(Model model, @PathVariable(value = "page") int page) {
+        List<Battery> list = batteryService.findAll();
+        pages = list.size() % PAGENUMBER != 0 ? list.size() / PAGENUMBER + 1 : list.size() / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = list.size() > (first + PAGENUMBER) ? first + PAGENUMBER : list.size();
+        list = list.subList(first, last);
+        model.addAttribute("uri", "/battery_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("batteryList", list);
         return "resultSet";
     }
 }
--- src/main/java/com/RacingDroneWIKI/controller/SearchByModel.java
+++ src/main/java/com/RacingDroneWIKI/controller/SearchByModel.java
@@ -5,9 +5,11 @@ import com.RacingDroneWIKI.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,33 +18,106 @@ import java.util.List;
  */
 @Controller
 public class SearchByModel {
+    final int PAGENUMBER = 10;
     @Autowired
     Search search;
-    @RequestMapping(value = "/search")
-    public String search(@RequestParam("searchModel") String searchModel, Model model){
-        List<Moto> motoList =new LinkedList<>();
-        List<ElectroSpeedRegulator> electroSpeedRegulatorList=new LinkedList<>();
-        List<Frame> frameList =new LinkedList<>();
-        List<ImageTransmission> imageTransmissionList=new LinkedList<>();
-        List<Cam> camList=new LinkedList<>();
-        List<FlightControl> flightControlList=new LinkedList<>();
-        List<Antenna> antennaList=new LinkedList<>();
-        List<PowerHub> powerHubList=new LinkedList<>();
-        List<Prop> propList=new LinkedList<>();
-        List<Battery> batteryList=new LinkedList<>();
-        search.searchModel(searchModel,motoList,electroSpeedRegulatorList,frameList,
-                imageTransmissionList,camList,flightControlList,antennaList,
-                powerHubList,propList,batteryList);
-        model.addAttribute("motoList",motoList);
-        model.addAttribute("electroSpeedRegulatorList",electroSpeedRegulatorList);
-        model.addAttribute("frameList",frameList);
-        model.addAttribute("imageTransmissionList",imageTransmissionList);
-        model.addAttribute("camList",camList);
-        model.addAttribute("flightControlList",flightControlList);
-        model.addAttribute("antennaList",antennaList);
-        model.addAttribute("powerHubList",powerHubList);
-        model.addAttribute("propList",propList);
-        model.addAttribute("batteryList",batteryList);
+
+    @RequestMapping(value = "/search_{page}")
+    public String search(@RequestParam("searchModel") String searchModel, Model model, @PathVariable(value = "page") int page) {
+        List<Moto> motoList = new LinkedList<>();
+        List<ElectroSpeedRegulator> electroSpeedRegulatorList = new LinkedList<>();
+        List<Frame> frameList = new LinkedList<>();
+        List<ImageTransmission> imageTransmissionList = new LinkedList<>();
+        List<Cam> camList = new LinkedList<>();
+        List<FlightControl> flightControlList = new LinkedList<>();
+        List<Antenna> antennaList = new LinkedList<>();
+        List<PowerHub> powerHubList = new LinkedList<>();
+        List<Prop> propList = new LinkedList<>();
+        List<Battery> batteryList = new LinkedList<>();
+        search.searchModel(searchModel, motoList, electroSpeedRegulatorList, frameList,
+                imageTransmissionList, camList, flightControlList, antennaList,
+                powerHubList, propList, batteryList);
+        int pages;
+        int first;
+        int last;
+        int size = motoList.size() + electroSpeedRegulatorList.size()
+                + frameList.size() + imageTransmissionList.size() +
+                camList.size() + flightControlList.size() + antennaList.size()
+                + powerHubList.size() + propList.size() + batteryList.size();
+
+        pages = size % PAGENUMBER != 0 ? size / PAGENUMBER + 1 : size / PAGENUMBER;
+        first = (page - 1) * PAGENUMBER;
+        last = size > (first + PAGENUMBER) ? first + PAGENUMBER : size;
+        List<Item> list = new LinkedList();
+        list.addAll(motoList);
+        list.addAll(electroSpeedRegulatorList);
+        list.addAll(frameList);
+        list.addAll(imageTransmissionList);
+        list.addAll(camList);
+        list.addAll(flightControlList);
+        list.addAll(antennaList);
+        list.addAll(powerHubList);
+        list.addAll(propList);
+        list.addAll(batteryList);
+        list = list.subList(first, last);
+        motoList.clear();
+        electroSpeedRegulatorList.clear();
+        frameList.clear();
+        imageTransmissionList.clear();
+        camList.clear();
+        flightControlList.clear();
+        antennaList.clear();
+        powerHubList.clear();
+        propList.clear();
+        batteryList.clear();
+        Iterator<Item> itemIterator = list.iterator();
+        while (itemIterator.hasNext()) {
+            Item item = itemIterator.next();
+            if (item instanceof Moto) {
+                motoList.add((Moto) item);
+            }
+            if (item instanceof ElectroSpeedRegulator) {
+                electroSpeedRegulatorList.add((ElectroSpeedRegulator) item);
+            }
+            if (item instanceof Frame) {
+                frameList.add((Frame) item);
+            }
+            if (item instanceof ImageTransmission) {
+                imageTransmissionList.add((ImageTransmission) item);
+            }
+            if (item instanceof Cam) {
+                camList.add((Cam) item);
+            }
+            if (item instanceof FlightControl) {
+                flightControlList.add((FlightControl) item);
+            }
+            if (item instanceof Antenna) {
+                antennaList.add((Antenna) item);
+            }
+            if (item instanceof PowerHub) {
+                powerHubList.add((PowerHub) item);
+            }
+            if (item instanceof Prop) {
+                propList.add((Prop) item);
+            }
+            if (item instanceof Battery) {
+                batteryList.add((Battery) item);
+            }
+        }
+        model.addAttribute("uri", "/search_");
+        model.addAttribute("pages", pages);
+        model.addAttribute("page", page);
+        model.addAttribute("searchModel", "?searchModel=" + searchModel);
+        model.addAttribute("motoList", motoList);
+        model.addAttribute("electroSpeedRegulatorList", electroSpeedRegulatorList);
+        model.addAttribute("frameList", frameList);
+        model.addAttribute("imageTransmissionList", imageTransmissionList);
+        model.addAttribute("camList", camList);
+        model.addAttribute("flightControlList", flightControlList);
+        model.addAttribute("antennaList", antennaList);
+        model.addAttribute("powerHubList", powerHubList);
+        model.addAttribute("propList", propList);
+        model.addAttribute("batteryList", batteryList);
         return "resultSet";
     }
 }
