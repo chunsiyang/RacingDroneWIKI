09dfebe394b3510badef5c61edbc4fb47ab456f2
chunsi.yang@hotmail.com <chunsi.yang@hotmail.com>
2018-06-01 08:38:57 +0800

    添加redis资源文件

--- src/main/java/com/RacingDroneWIKI/spring/config/RedisConfig.java
+++ src/main/java/com/RacingDroneWIKI/spring/config/RedisConfig.java
@@ -1,23 +1,39 @@
 package com.RacingDroneWIKI.spring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Properties;
 
+@PropertySource("classpath:redis.properties")
 @EnableRedisHttpSession
 public class RedisConfig {
 
+    @Value("${redis.host}")
+    private String host;
+    @Value("${redis.port}")
+    private int port;
+    @Value("${redis.pass}")
+    private String pass;
+    @Value("${redis.timeout}")
+    private int timeout;
     @Bean
     public JedisConnectionFactory redisConnectionFactory()
     {
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        jedisConnectionFactory.setHostName("127.0.0.1");
-        jedisConnectionFactory.setPort(6379);
-        jedisConnectionFactory.setPassword("");
-        jedisConnectionFactory.setTimeout(3000);
+        jedisConnectionFactory.setHostName(host);
+        jedisConnectionFactory.setPort(port);
+        jedisConnectionFactory.setPassword(pass);
+        jedisConnectionFactory.setTimeout(timeout);
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
         jedisPoolConfig.setMaxIdle(10);
         jedisPoolConfig.setMaxTotal(100);
